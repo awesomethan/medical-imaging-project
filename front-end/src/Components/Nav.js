7be4ff03771851d89d43d05ba0442971fdd7f7cb
awesomethan <89287311+awesomethan@users.noreply.github.com>
2023-01-11 22:15:09 -0500

    Update Nav.js

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -9,13 +9,13 @@ function Nav() {
 
   return (
     <nav>
-      <div className="nav-links">
+      <div className="nav-objects d-flex justify-content-between align-items-center">
         <img
           src="images/logo.png"
           alt="img cannot be displayed"
           className="logo"
         ></img>
-        <Button variant="info" onClick={handleShow}>
+        <Button variant="info" className="text-white" onClick={handleShow}>
           About
         </Button>
         <Modal show={show} onHide={handleClose}>
@@ -25,11 +25,15 @@ function Nav() {
           <Modal.Body>
             <p>
               Welcome to our medical imaging site! Here, you can upload an image
-              and we will tell you ... It has an accuracy of
+              of a mole and we will tell you whether its cancerous or not. We
+              built this using a ML Model (for details check the GitHub
+              repository :D) that has an accuracy of 94%! We'd like to apologize
+              for the 6% of the time when you're told that you have cancer and a
+              week to live when you don't lol!
             </p>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="danger" onClick={handleClose}>
+            <Button variant="info" onClick={handleClose}>
               Understood!
             </Button>
           </Modal.Footer>
